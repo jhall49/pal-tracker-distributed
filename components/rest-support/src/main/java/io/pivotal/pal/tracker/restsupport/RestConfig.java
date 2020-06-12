84f192ec1af8278664562273f17ef922e6e4a83a
John Hall <jhall49@wowway.com>
2020-06-12 16:22:17 +0000

    Circuit Breaker

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+
 @Configuration
 public class RestConfig {
 
